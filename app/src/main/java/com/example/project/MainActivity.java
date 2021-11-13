684615abe1b500b27467e7af9cfc3f58ea7f0cf4
JMKim <kjmdkdlel@nate.com>
2021-11-13 20:52:22 +0900

    11.13 push loginpage,signuppage

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    TextView TV_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TV_signup = (TextView) findViewById(R.id.TV_signup);
+        TV_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupPage.class);
+                startActivity(intent);
+            }
+        });
+    };
+
     }
-}
+
