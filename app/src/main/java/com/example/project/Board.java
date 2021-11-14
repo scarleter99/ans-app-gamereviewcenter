49c52af6990c77da910d5422a8c4a1ce879fc4cf
xoqja99 <ddino99@naver.com>
2021-11-14 17:22:23 +0900

    네비바 추가

--- app/src/main/java/com/example/project/Board.java
+++ app/src/main/java/com/example/project/Board.java
@@ -9,5 +9,6 @@ public class Board extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.board);
     }
 }
