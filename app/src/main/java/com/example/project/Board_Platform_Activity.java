f1162291e18e4e64b811fa2f65db6affbcd24d91
JMKim <kjmdkdlel@nate.com>
2021-11-19 02:19:51 +0900

    11.19 일부 UI 개선

--- app/src/main/java/com/example/project/Board_Platform_Activity.java
+++ app/src/main/java/com/example/project/Board_Platform_Activity.java
@@ -1,16 +1,29 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Board_Platform_Activity extends AppCompatActivity {
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_platform);
+        //상단 바 제거
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        Button BTN_Sample = (Button) findViewById(R.id.BTN_samplereview);
+        BTN_Sample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
